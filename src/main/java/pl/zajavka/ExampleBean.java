65250f1dc33fd532612a55007226a64f126feec0
Kubanczyk6 <jakub.gradzki96@gmail.com>
2023-03-31 22:34:13 +0200

    first commit Annotation Config Application Context

---
+++ src/main/java/pl/zajavka/ExampleBean.java
@@ -0,0 +1,17 @@
+package pl.zajavka;
+
+public class ExampleBean {
+
+    private final InjectedBean injectedBean;
+    private final AnotherInjectedBean anotherInjectedBean;
+
+    public ExampleBean(InjectedBean injectedBean, AnotherInjectedBean anotherInjectedBean) {
+        this.injectedBean = injectedBean;
+        this.anotherInjectedBean = anotherInjectedBean;
+        System.out.println("Calling ExampleBean constructor");
+    }
+
+    public void exampleMethod(){
+        System.out.println("Calling exampleMethod");
+    }
+}
